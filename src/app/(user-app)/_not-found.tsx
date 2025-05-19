f0e1f932a118c7a4068669432cabd92cd7d35bbe
Evgeny Evseev <evgeny.evseev@vk.team>
2025-05-19 12:09:27 +0300

    fix build

--- src/app/(user-app)/_not-found.tsx
+++ src/app/(user-app)/_not-found.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import "./globals.css";
 import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
